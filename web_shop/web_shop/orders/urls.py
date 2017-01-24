f681fa21b162c1b9129903ab1e6529baaddb161d
klimets <igor35hh@gmail.com>
2017-01-24 15:18:16 +0200

    export to csv and print detail information of orders

--- web_shop/web_shop/orders/urls.py
+++ web_shop/web_shop/orders/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^create/$', views.OrderCreate, name='OrderCreate')
+    url(r'^create/$', views.OrderCreate, name='OrderCreate'),
+    url(r'^admin/order/(?P<order_id>\d+)/$', views.AdminOrderDetail, name='AdminOrderDetail')
 ]
--- web_shop/web_shop/orders/views.py
+++ web_shop/web_shop/orders/views.py
@@ -5,6 +5,14 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from django.shortcuts import get_object_or_404
+from .models import Order
+from django.contrib.admin.views.decorators import staff_member_required
+
+@staff_member_required
+def AdminOrderDetail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
 
 def OrderCreate(request):
     cart = Cart(request)
